7b52efbeb687aa9767f5d591c40d2bd5c3a3d211
罗旭宏 <barntet@163.com>
2022-08-05 09:48:53 +0800

    complete the should make nested properties reactive test of ref

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,35 +1,55 @@
-import { hasChange } from './../shared/index';
+import { hasChange, isObject } from './../shared/index';
 import { trackEffects, triggerEffects, isTracting } from './effect';
+import { reactive } from './reactive';
 
 class RefImpl {
-	// private _rawValue: any;
+	// 核心点，一个key对应一个dep,
+	// ref和reactive的区别，ref传入的都是单值，reactive是一个对象
+	// 怎么知道被get set， 所以通过一个class包裹
+
 	private _value: any;
 	public dep;
+	private _rawValue: any;
 
 	constructor(value) {
-		this._value = value;
+		// 存储传入的原始值，因为_value如果是对象会经过reactive包裹
+		this._rawValue = value;
+		// 看看value是不是一个对象，如果是对象用reactive包裹一下
+		this._value = convert(value);
+
 		// 实例化dep
 		this.dep = new Set();
 	}
 
 	get value() {
-		// activeEffect不等于undefined，才调用收集
-		if (isTracting()) {
-			// 收集依赖
-			trackEffects(this.dep);
-		}
+		trackRefValue(this);
 		return this._value;
 	}
 
 	set value(neWValue) {
 		// hasChange
 		if (hasChange(neWValue, this._value)) {
+			// 更新值 ,这里顺序和get相反
+			this._value = convert(neWValue);
+			this._rawValue = neWValue;
 			// 一定是先去修改了value的值，再去触发依赖
-			this._value = neWValue;
 			triggerEffects(this.dep);
 		}
 	}
 }
+
+function convert(value) {
+	return isObject(value) ? reactive(value) : value;
+}
+
+function trackRefValue(ref) {
+	// activeEffect不等于undefined，才调用收集
+	if (isTracting()) {
+		// 收集依赖
+		trackEffects(ref.dep);
+	}
+}
+
 export function ref(value) {
 	const refImpl = new RefImpl(value);
 	return refImpl;
